2b383f7e2e85ac09e70d400ae6800f0d5f3c91a3
VLIGS <liudmila.veshneva@gmail.com>
2016-04-24 22:05:08 +0100

    added tests for addEdge and made sure they failed

--- src/roadgraph/testMapGraph.java
+++ src/roadgraph/testMapGraph.java
@@ -25,4 +25,69 @@ public class testMapGraph {
         myTestGraph.addVertex(location);
         assertEquals(false, myTestGraph.addVertex(location));
     }
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddEdgeGeographicPoint1Null(){
+        MapGraph myTestGraph = new MapGraph();
+        GeographicPoint to = new GeographicPoint(53.01799, -2.1675);
+        myTestGraph.addEdge(null, to, "main street", "residential", 23.1 );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddEdgeGeographicPoint2Null(){
+        MapGraph myTestGraph = new MapGraph();
+        GeographicPoint from = new GeographicPoint(53.01799, -2.1675);
+        myTestGraph.addEdge(from, null, "main street", "residential", 23.1 );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddEdgeGeographicRoadNameNull(){
+        MapGraph myTestGraph = new MapGraph();
+        GeographicPoint from = new GeographicPoint(53.01799, -2.1675);
+        GeographicPoint to = new GeographicPoint(52.47, -1.9706);
+        myTestGraph.addEdge(from, to, null, "residential", 23.1 );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddEdgeGeographicRoadTypeNull(){
+        MapGraph myTestGraph = new MapGraph();
+        GeographicPoint from = new GeographicPoint(53.01799, -2.1675);
+        GeographicPoint to = new GeographicPoint(52.47, -1.9706);
+        myTestGraph.addEdge(from, to, "main street", null, 23.1 );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddEdgeGeographicLengthNull(){
+        MapGraph myTestGraph = new MapGraph();
+        GeographicPoint from = new GeographicPoint(53.01799, -2.1675);
+        GeographicPoint to = new GeographicPoint(52.47, -1.9706);
+        myTestGraph.addEdge(from, to, "main street", "residential", 0.0 );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddEdgeLengthNegative(){
+        MapGraph myTestGraph = new MapGraph();
+        GeographicPoint from = new GeographicPoint(53.01799, -2.1675);
+        GeographicPoint to = new GeographicPoint(52.47, -1.9706);
+        myTestGraph.addEdge(from, to, "main street", "residential", -23.1 );
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddEdgeGeographicPoint1NotInGraph(){
+        MapGraph myTestGraph = new MapGraph();
+        GeographicPoint from = new GeographicPoint(53.01799, -2.1675);
+        GeographicPoint to = new GeographicPoint(52.47, -1.9706);
+        myTestGraph.addVertex(to);
+        myTestGraph.addEdge(from, to, "main street", "residential", 23.1 );
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddEdgeGeographicPoint2NotInGraph(){
+        MapGraph myTestGraph = new MapGraph();
+        GeographicPoint from = new GeographicPoint(53.01799, -2.1675);
+        GeographicPoint to = new GeographicPoint(52.47, -1.9706);
+        myTestGraph.addVertex(from);
+        myTestGraph.addEdge(from, to, "main street", "residential", 23.1 );
+    }
+
 }
